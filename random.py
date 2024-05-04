b825dc97b9f1318c8682b6230c041c8babde1a47
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-05 00:10:32 +0600

    Add files via upload

---
+++ random.py
@@ -0,0 +1,59 @@
+#two number sum
+x = 10
+y = 20
+sum = x+y
+print(sum)
+#two string sum
+x = "sajib"
+y = "ahmed"
+print(x+y)
+
+# type casting
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+# data type
+print(type(x))
+print(type(y))
+print(type(z))
+
+#multi varibale:
+x,y,z,a,b = "sajib","Rakib","Emran","Habib","Akash"
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+x,y,z,a,b = 10,20,30,40,50
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+
+#list
+li = ["messi","Neymeer","Ronaldo","Mbappe"]
+print(li)
+print(type(li))
+
+a,b,c,d = li
+x,y,z,a,b = "sajib","Rakib","Emran","Habib","Akash"
+print(a)
+print(b)
+print(c)
+print(d)
+#ouput Variable :
+x = "I am student"
+print(x)
+x = "sajib"
+y = "ahmed"
+z = "student"
+print(x,y,z)
+print(x+y+z)
+
