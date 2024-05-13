d6ea5bc0448fba0148db987e5a6cfcb3410fb4a1
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-14 00:54:53 +0600

    Add files via upload

---
+++ stringmethod.py
@@ -0,0 +1,42 @@
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+txt = "36 is my age."
+
+x = txt.capitalize()
+
+print (x)
+num1 = input()
+print(num1)
+print(type(num1))
+
+num1 = int(input())
+print(num1)
+
+num1 = int(input())
+num2 = int(input())
+result = (num1+num2)
+print(result)
+print(type(result))
+
+result = (num1-num2)
+print(result)
+print(type(result))
+
+result = (num1*num2)
+print(result)
+print(type(result))
+
+result = (num1/num2)
+print(result)
+print(type(result))
+
+result = (num1//num2)
+print(result)
+print(type(result))
+
+result = (num1%num2)
+print(result)
+print(type(result))
