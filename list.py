9cf16d9703c1bcdb84b45408dc913acd4c77aec5
Sajib Ahmed <112968850+mollah2022@users.noreply.github.com>
2024-05-15 01:07:41 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,117 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+      print(thislist[i])
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+#New topic
+print("NEW TOPICES : ")
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key = str.lower)
+print(thislist)
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
